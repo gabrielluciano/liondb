4757a98d8d11c3d7c184b7d43fdf0499f7819ab5
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-03-06 17:57:11 -0300

    test: add AssertNotNil method

--- internal/testutil/testutil.go
+++ internal/testutil/testutil.go
@@ -26,6 +26,17 @@ func AssertNil(tb testing.TB, actual interface{}, name string) {
 	tb.Errorf("Incorrect result, expected %s to be '<nil>', got '%v'", name, actual)
 }
 
+func AssertNotNil(tb testing.TB, actual interface{}, name string) {
+	if actual == nil {
+		tb.Errorf("Incorrect result, expected %s to be not '<nil>', got '%v'", name, actual)
+	}
+
+	rv := reflect.ValueOf(actual)
+	if rv.Kind() == reflect.Pointer && rv.IsNil() {
+		tb.Errorf("Incorrect result, expected %s to be not '<nil>', got '%v'", name, actual)
+	}
+}
+
 func AssertEquals(tb testing.TB, expected, actual interface{}, name string) {
 	if expected != actual {
 		tb.Errorf("Incorrect result, expected %s to be '%v', got '%v'", name, expected, actual)
